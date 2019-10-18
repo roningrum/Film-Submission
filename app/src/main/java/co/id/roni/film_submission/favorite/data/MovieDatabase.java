9cf063473225072bac51657195a8f309c9f5469b
rachmad.alif <rachmad@garasilabs.com>
2019-10-18 21:16:37 +0700

    Benerin Bug Pada Movie Database

--- app/src/main/java/co/id/roni/film_submission/favorite/data/MovieDatabase.java
+++ app/src/main/java/co/id/roni/film_submission/favorite/data/MovieDatabase.java
@@ -13,43 +13,16 @@ import co.id.roni.film_submission.favorite.movie.MovieFavModel;
 
 @Database(entities = {MovieFavModel.class}, version = 1)
 public abstract class MovieDatabase extends RoomDatabase {
-    private static volatile MovieDatabase INSTANCE;
-    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback() {
-        @Override
-        public void onOpen(@NonNull SupportSQLiteDatabase db) {
-            super.onOpen(db);
-            new PopulateDbAsync(INSTANCE).execute();
-        }
-    };
-
+    public abstract MovieDao movieDao();
+    public static MovieDatabase instance = null;
     public static MovieDatabase getDatabase(Context context) {
-        if (INSTANCE == null) {
+        if (instance == null) {
             synchronized (MovieDatabase.class) {
-                INSTANCE = Room.databaseBuilder(context, MovieDatabase.class, "movie_db")
-                        .addCallback(sRoomDatabaseCallback)
+                instance = Room.databaseBuilder(context, MovieDatabase.class, "movie_db")
+                        .allowMainThreadQueries()
                         .build();
             }
         }
-        return INSTANCE;
+        return instance;
     }
-
-    public abstract MovieDao movieDao();
-
-    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
-        private final MovieDao movieDao;
-
-        PopulateDbAsync(MovieDatabase instance) {
-            movieDao = instance.movieDao();
-        }
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            movieDao.deleteAll();
-            MovieFavModel movieFavModel = new MovieFavModel();
-            movieDao.insert(movieFavModel);
-//            movieDao.isFavorite(movieFavModel.getId());
-            return null;
-        }
-    }
-
 }
