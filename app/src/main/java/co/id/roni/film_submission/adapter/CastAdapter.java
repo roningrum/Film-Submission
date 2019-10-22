05cc899c762f59c6f51827e9e95958a245caddb7
Rizki Oktavia Ningrum <rizkioktavianingrum@gmail.com>
2019-10-23 05:11:17 +0700

    Add Cast Adapter and item_credit

--- app/src/main/java/co/id/roni/film_submission/adapter/CastAdapter.java
+++ app/src/main/java/co/id/roni/film_submission/adapter/CastAdapter.java
@@ -1,4 +1,61 @@
 package co.id.roni.film_submission.adapter;
 
-public class CastAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import co.id.roni.film_submission.R;
+import co.id.roni.film_submission.model.Cast;
+
+public class CastAdapter extends RecyclerView.Adapter<CastAdapter.CastMovieViewHolder> {
+    private ArrayList<Cast> castMovieList;
+
+    public CastAdapter(ArrayList<Cast> castMovieList) {
+        this.castMovieList = castMovieList;
+    }
+
+    @NonNull
+    @Override
+    public CastAdapter.CastMovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new CastMovieViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_credit_cast, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CastAdapter.CastMovieViewHolder holder, int position) {
+        holder.bind(castMovieList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return castMovieList.size();
+    }
+
+
+    class CastMovieViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.tv_cast)
+        TextView movieCastName;
+        @BindView(R.id.img_cast)
+        ImageView imgMovieCast;
+
+        CastMovieViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        void bind(Cast cast) {
+            movieCastName.setText(cast.getName());
+            Glide.with(itemView.getContext()).load(cast.getProfile_path()).into(imgMovieCast);
+        }
+    }
 }
