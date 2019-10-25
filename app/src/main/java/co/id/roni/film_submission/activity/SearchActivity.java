d1547e2e658fdae79c033c83362b2f156a06f3fd
Rizki Oktavia Ningrum <rizkioktavianingrum@gmail.com>
2019-10-26 01:24:15 +0700

    Modified Search

--- app/src/main/java/co/id/roni/film_submission/activity/SearchActivity.java
+++ app/src/main/java/co/id/roni/film_submission/activity/SearchActivity.java
@@ -1,16 +1,49 @@
 package co.id.roni.film_submission.activity;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 
+import java.util.Objects;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import co.id.roni.film_submission.R;
 
 public class SearchActivity extends AppCompatActivity {
+    @BindView(R.id.toolbar)
+    Toolbar searchToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(searchToolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_menu, menu);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) (menu.findItem(R.id.action_search)).getActionView();
+        if (searchManager != null) {
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+            searchView.setIconified(false);
+            searchView.setQueryHint(getString(R.string.key_search));
+            searchView.onActionViewExpanded();
+
+        }
+
+        return super.onCreateOptionsMenu(menu);
+
     }
 }
