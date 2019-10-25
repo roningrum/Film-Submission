716ea89070ab34d249085d927b41781759c701fb
Rizki Oktavia Ningrum <rizkioktavianingrum@gmail.com>
2019-10-26 02:17:45 +0700

    Fix Api and add Recyclerview for Movie Result

--- app/src/main/java/co/id/roni/film_submission/service/Api.java
+++ app/src/main/java/co/id/roni/film_submission/service/Api.java
@@ -12,25 +12,25 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface Api {
-    String BASE_URL = "https://api.themoviedb.org/";
+    String BASE_URL = "https://api.themoviedb.org/3/";
 
 
-    @GET("3/discover/movie?api_key=" + BuildConfig.API_KEY + "&sort_by=popularity.desc&include_adult=false&include_video=false")
+    @GET("discover/movie?api_key=" + BuildConfig.API_KEY + "&sort_by=popularity.desc&include_adult=false&include_video=false")
     Call<MovieObjectData> getMovieList(@Query("id") int Id, @Query("language") String language);
 
-    @GET("3/discover/tv?api_key=" + BuildConfig.API_KEY + "&sort_by=popularity.desc&include_adult=false&include_video=false")
+    @GET("discover/tv?api_key=" + BuildConfig.API_KEY + "&sort_by=popularity.desc&include_adult=false&include_video=false")
     Call<TvShowsObject> getTVList(@Query("id") int Id, @Query("language") String language);
 
-    @GET("/3/movie/{movie_id}?api_key=" + BuildConfig.API_KEY)
+    @GET("movie/{movie_id}?api_key=" + BuildConfig.API_KEY)
     Call<MovieDetailModel> getMovieDetail(@Path("movie_id") int Id, @Query("language") String language);
 
-    @GET("/3/tv/{tv_id}?api_key=" + BuildConfig.API_KEY)
+    @GET("tv/{tv_id}?api_key=" + BuildConfig.API_KEY)
     Call<TVShowDetailModel> getTvShowDetail(@Path("tv_id") int Id, @Query("language") String language);
 
-    @GET("/3/movie/{movie_id}/credits?api_key=" + BuildConfig.API_KEY)
+    @GET("movie/{movie_id}/credits?api_key=" + BuildConfig.API_KEY)
     Call<CreditObjectData> getCastMovieList(@Path("movie_id") int Id, @Query("api") String api);
 
-    @GET("/3/tv/{tv_id}/credits?api_key=" + BuildConfig.API_KEY)
+    @GET("tv/{tv_id}/credits?api_key=" + BuildConfig.API_KEY)
     Call<CreditObjectData> getCastTvList(@Path("tv_id") int Id, @Query("api") String api);
 
 }
