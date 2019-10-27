1a2421ffd7500c5b92a47376efb356e48d061b5f
Rizki Oktavia Ningrum <rizkioktavianingrum@gmail.com>
2019-10-27 17:40:11 +0700

    Add Fragment for Consumer App

---
+++ moviefavoriteapp/src/main/java/co/id/roni/moviefavoriteapp/MovieFavoriteFragment.java
@@ -0,0 +1,34 @@
+package co.id.roni.moviefavoriteapp;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+
+public class MovieFavoriteFragment extends Fragment {
+    public MovieFavoriteFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movie_favorite, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
