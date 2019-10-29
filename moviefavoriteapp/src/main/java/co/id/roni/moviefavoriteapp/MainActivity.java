256ed0570b98a7194143de3edd84e52953a1b384
Rizki Oktavia Ningrum <rizkioktavianingrum@gmail.com>
2019-10-30 05:16:59 +0700

    Add Tab Layout

--- moviefavoriteapp/src/main/java/co/id/roni/moviefavoriteapp/MainActivity.java
+++ moviefavoriteapp/src/main/java/co/id/roni/moviefavoriteapp/MainActivity.java
@@ -1,40 +1,26 @@
 package co.id.roni.moviefavoriteapp;
 
 import android.annotation.SuppressLint;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.google.android.material.tabs.TabLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import co.id.roni.moviefavoriteapp.tab.ViewPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String PROVIDER_NAME = "co.id.roni.film_submission";
-    private static final String TABLE_NAME = "tbMovieFav";
-    private static final String URL = "content://" + PROVIDER_NAME + "/" + TABLE_NAME;
-    private static final Uri CONTENT_URI = Uri.parse(URL);
-    @BindView(R.id.rv_movies_favs)
-    RecyclerView rvMovieFavConsumer;
-    private MovieFavoriteAdapter movieFavoriteAdapter;
 
-
-    private Observer<List<MovieModel>> getMovieFavs = new Observer<List<MovieModel>>() {
-        @Override
-        public void onChanged(List<MovieModel> movieModels) {
-            if (movieModels != null) {
-                movieFavoriteAdapter.setMovieFavModels(movieModels);
-            }
-        }
-    };
+    @BindView(R.id.tabs)
+    TabLayout tabFavoriteConsumer;
+    @BindView(R.id.viewpage)
+    ViewPager viewPager;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @SuppressLint("Recycle")
     @Override
@@ -43,15 +29,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        movieFavoriteAdapter = new MovieFavoriteAdapter(new ArrayList<>());
-        rvMovieFavConsumer.setAdapter(movieFavoriteAdapter);
-        rvMovieFavConsumer.setLayoutManager(new LinearLayoutManager(this));
-        rvMovieFavConsumer.setHasFixedSize(true);
+        ViewPagerAdapter favPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        favPagerAdapter.addFragment(new MovieFragment(), getString(R.string.movie));
+        favPagerAdapter.addFragment(new TvShowFragment(), getString(R.string.tvshow));
+        viewPager.setAdapter(favPagerAdapter);
+        tabFavoriteConsumer.setupWithViewPager(viewPager);
+        toolbar.setTitle(getString(R.string.Favorite));
+        toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
 
-        Cursor cursor = getApplicationContext().getContentResolver().query(CONTENT_URI, null, null, null, null);
-        MovieFavoriteViewModel movieViewModel = ViewModelProviders.of(this).get(MovieFavoriteViewModel.class);
-        movieViewModel.getListMovieFavs().observe(this, getMovieFavs);
-        movieViewModel.setListMovieFavs(cursor);
+
 
 
     }
